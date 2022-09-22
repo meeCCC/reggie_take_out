3eaa3e78ac763f019dd7b8d6ca2521d8ee91dde6
cjw <763145370@qq.com>
2022-09-22 20:20:32 +0800

    分页查询拦截器

---
+++ src/main/java/com/cjw/reggie/config/MybatisPlusConfig.java
@@ -0,0 +1,23 @@
+package com.cjw.reggie.config;
+
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class MybatisPlusConfig {
+
+    /**
+     * 分页查询
+     * @return
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return mybatisPlusInterceptor;
+    }
+}
